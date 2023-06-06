75cda56f90171a883d8a012fd7d028e08f01e14d
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-06-06 21:46:51 +0900

    fix: change database setting

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_06_113205) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
