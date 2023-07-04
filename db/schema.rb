8b1aee219a606d510dc6f210a498e7cc5a6499cf
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-07-04 18:36:48 +0900

    add test models

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_113205) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_073928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
